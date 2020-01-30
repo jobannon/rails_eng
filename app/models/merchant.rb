eadbb4adda7b74ac4944179ea933f7c1624a9924
Joshua O'bannon <jtobannon@gmail.com>
2020-01-29 19:34:50 -0700

    Update Merchant model and spec

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+
+  has_many :items
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
 end 
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Merchant, type: :model do
   describe "relationships" do 
     it { should have_many :items}
     it { should have_many :invoices}
+
+    xit { should have_many }
   end
 end
