7828f331b0be5328abf57d0f559422388cdc225b
Joshua O'bannon <jtobannon@gmail.com>
2020-01-29 20:12:18 -0700

    Adds Find_all functionality to Merchant Endpoint Updated Tests and Controller

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::Merchants::FindController < ApplicationController
     render json: MerchantSerializer.new(Merchant.find_by(request.query_parameters)) 
   end
 
+  def index 
+##pull our cohntroller logic
+    render json: MerchantSerializer.new(Merchant.where(request.query_parameters)) 
+    # render json: MerchantSerializer.new(Merchant.find(request.query_parameters)) 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do  
         get '/find', to: "find#show"
+        get '/find_all', to: "find#index"
       end
 
       resources :merchants, only: [:index, :show] do 
