7b05a25297240723316028df391ed0895f756375
Joshua O'bannon <jtobannon@gmail.com>
2020-01-30 14:41:30 -0700

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,20 @@ Rails.application.routes.draw do
         get '/find', to: "find#show"
         get '/find_all', to: "find#index"
         get '/random', to: "findrand#show"
+
+        get '/:id/items', to: "merchantitems#index"
+      end
+      namespace :invoices do 
       end
 
+      resources :invoices, only: [:index, :show] do 
+        resources :transactions, only: [:index]
+      end
       resources :merchants, only: [:index, :show] do 
         resources :items, only: [:index]
         resources :invoices, only: [:index]
       end
+
       get "/merchants/most_revenue", to: "merchants#most_revenue"  
 
     end
