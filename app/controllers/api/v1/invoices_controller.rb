830b22cccaddd90c424c7c93b469e8bbc25b2ffe
Joshua O'bannon <jtobannon@gmail.com>
2020-01-30 22:55:54 -0700

    Add show to invoice controller

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::InvoicesController < ApplicationController
   def index
     render json: InvoiceSerializer.new(Merchant.find(params[:merchant_id]).invoices) 
   end
+
+  def show
+    render json: InvoiceSerializer.new(Invoice.find(params[:id])) 
+  end
 end
