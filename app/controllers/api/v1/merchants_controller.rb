7cfa70726b3c2fca37a31c71d89100d659fd52c3
Joshua O'bannon <jtobannon@gmail.com>
2020-02-02 16:11:43 -0700

    Remove unrestful route

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,8 +8,4 @@ class Api::V1::MerchantsController < ApplicationController
     merchant = Merchant.find(params[:id])
     render json: MerchantSerializer.new(merchant)
  end
-
-  def most_revenue
-    result = Merchant.joins(:invoice_items).select("items.*, sum(invoice_items.quantity as quantity").group(:id).order("quantity desc").limit(params[:quantity])
-  end
 end
