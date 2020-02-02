9cfe0750d4ffafc83a192341f9c27aeaabcaa18b
Joshua O'bannon <jtobannon@gmail.com>
2020-02-02 16:10:36 -0700

    Add find controlelr for invoice items

--- app/controllers/api/v1/invoice_items/find_controller.rb
+++ app/controllers/api/v1/invoice_items/find_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::InvoiceItems::FindController < ApplicationController
 
   def show
-    render json: InvoiceItemsSerializer.new(InvoiceItem.find_by(request.query_parameters)) 
+    render json: InvoiceItemsSerializer.new(InvoiceItem.find_by(find_params)) 
   end
 
   def index 
-    render json: InvoiceItemsSerializer.new(InvoiceItem.find_all(request.query_parameters)) 
+    render json: InvoiceItemsSerializer.new(InvoiceItem.find_all(find_params)) 
+  end
+
+  def find_params
+    params.permit(:id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at)
   end
 end
