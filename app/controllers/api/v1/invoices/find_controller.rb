ea883110636915e22378bd1264a60da9a5e4e8f7
Joshua O'bannon <jtobannon@gmail.com>
2020-02-02 16:10:47 -0700

    Add find controller for invoices

--- app/controllers/api/v1/invoices/find_controller.rb
+++ app/controllers/api/v1/invoices/find_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::Invoices::FindController < ApplicationController
 
   def show
-    render json: InvoiceSerializer.new(Invoice.find_by(request.query_parameters)) 
+    render json: InvoiceSerializer.new(Invoice.find_by(find_params)) 
   end
 
   def index 
-    render json: InvoiceSerializer.new(Invoice.find_all(request.query_parameters)) 
+    render json: InvoiceSerializer.new(Invoice.find_all(find_params)) 
+  end
+
+  private 
+
+  def find_params
+    params.permit(:id, :invoice_id, :customer_id , :status, :updated_at, :created_at)
   end
 end
