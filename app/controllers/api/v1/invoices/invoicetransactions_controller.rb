ba1088e97fbe02bed0c595b3b9861074b235fb86
Joshua O'bannon <jtobannon@gmail.com>
2020-02-02 16:11:53 -0700

    Add controller

--- app/controllers/api/v1/invoices/invoicetransactions_controller.rb
+++ app/controllers/api/v1/invoices/invoicetransactions_controller.rb
@@ -2,7 +2,7 @@
 class Api::V1::Invoices::InvoicetransactionsController < ApplicationController
 
   def show
-    render json: TransactionsSerializer.new(Invoice.find(params[:invoice_id]).transactions) 
+    render json: TransactionSerializer.new(Invoice.find(params[:invoice_id]).transactions) 
   end
 end
 
