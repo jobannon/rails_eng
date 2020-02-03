10fda8b56896d88f2299083058004e14e2c85851
Joshua O'bannon <jtobannon@gmail.com>
2020-02-03 00:13:23 -0700

    Comment off rando method for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,14 +6,17 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
 
-  def self.find_rand
-    # total_merchants = Merchant.all.count - 1
-    array_for_sampling = Array.new
-    Merchant.all.each do |merchant|
-      array_for_sampling << merchant.id
-    end
-    Merchant.find(array_for_sampling.sample)
-  end
+  # def self.find_rand
+  #   # total_merchants = Merchant.all.count - 1
+  #   array_for_sampling = Array.new
+  #   Merchant.all.each do |merchant|
+  #     array_for_sampling << merchant.id
+  #   end
+  #   Merchant.find(array_for_sampling.sample)
+  # end
+# SELECT floor(random()*(25-10+1))+10;
+#Model.order('RANDOM()').first
+# self.order('RANDOM()').first
 
   def self.find_with_order(id)
     items = Merchant.find(id).items
