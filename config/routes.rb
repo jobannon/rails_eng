180e873af73553327013d744e209d46b38b13c41
Joshua O'bannon <jtobannon@gmail.com>
2020-02-03 00:12:28 -0700

    Update routes and schmea

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
         get '/find', to: "find#show"
         get '/find_all', to: "find#index"
         get "/most_revenue",  to: "most_revenue#index"  
+        get '/random', to: "rando#show"
       end
       namespace :invoices do 
         get '/find', to: "find#show"
