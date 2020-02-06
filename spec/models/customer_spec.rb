61c97f447538b974fe8d3b7a1dd44a05b7578e78
Joshua O'bannon <jtobannon@gmail.com>
2020-02-06 16:31:24 -0700

    update customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'relationships' do 
-    xit {should have_many :invoices}
-    xit {should have_many :transactions}
+    it {should have_many :invoices}
+    it {should have_many :transactions}
   end
 end
